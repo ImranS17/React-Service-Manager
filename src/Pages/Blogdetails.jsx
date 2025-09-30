ace01b35e3c42c5d538a97bfe196102776115051
Imran S. <skimran0917@gmail.com>
2025-09-30 19:43:50 +0530

    Update Blogdetails.jsx

--- src/Pages/Blogdetails.jsx
+++ src/Pages/Blogdetails.jsx
@@ -1,6 +1,6 @@
 import { useParams, Link } from "react-router-dom";
 import { useEffect, useState } from "react";
-import "./BlogDetails.css";
+import "./Blogdetails.css";
 
 export default function BlogDetails() {
   const { id } = useParams();
